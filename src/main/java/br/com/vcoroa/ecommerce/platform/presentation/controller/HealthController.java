69aa3ebc0c129564fceafd04c355daa68acc6e81
Vanilton Coroa <vanilton.coelho@gmail.com>
2025-10-08 00:25:40 -0300

    commit inicial

---
+++ src/main/java/br/com/vcoroa/ecommerce/platform/presentation/controller/HealthController.java
@@ -0,0 +1,24 @@
+package br.com.vcoroa.ecommerce.platform.presentation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> health() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", "E-Commerce Platform is running!");
+        return ResponseEntity.ok(response);
+    }
+}
